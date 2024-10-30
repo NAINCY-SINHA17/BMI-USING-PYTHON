115e5d6073abe53af066619be2fe5e1369375b0f
HEY NAINCY <sinhasinu17@gmail.com>
2024-10-30 08:36:39 -0700

    Add files via upload
    This is a python project to find BMI (Body Mass Index) 
    The formula for calculating a person's body mass index is to 
    divide their weight (kg) by their height meter squared.

---
+++ BODY MASS INDEX.py
@@ -0,0 +1,11 @@
+#BODY MASS INDEX (BMI) CALCULATOR
+
+
+Weight_kg = float(input("Enter Weight in kg"))
+
+Height_meter = float(input("Enter Height in meters"))
+
+BMI = Weight_kg / (Height_meter * Height_meter)
+
+print("BMI is :" , BMI)
+
